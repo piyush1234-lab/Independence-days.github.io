c6f53afc7c09cd04326740110230952f1e9c85c4
Piyush <sagarwal2k20@gmail.com>
2025-07-25 12:50:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+      setTimeout(() => {
+        document.getElementById('image').classList.add('reveal');
+      }, 500); // Trigger image reveal after plane starts
+    });    
