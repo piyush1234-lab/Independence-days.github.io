cf79eb2f1ff8ff4a3f0c63f92c4392dd8b96caa0
Piyush <sagarwal2k20@gmail.com>
2025-07-28 12:58:18 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', () => {
-      setTimeout(() => {
-        document.getElementById('image').classList.add('reveal');
-      }, 500); // Trigger image reveal after plane starts
-    });    
